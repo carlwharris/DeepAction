4893103cc8e39f6108327aa24b0b2ddbeefb9a7f
carlwharris <50850205+carlwharris@users.noreply.github.com>
2022-06-24 12:08:14 -0400

    function documentation & user input improvement
    - Warns user if project already exists
    - If config file exists, asks user whether to overwrite

--- DeepAction/@DeepActionProject/CreateProject.m
+++ DeepAction/@DeepActionProject/CreateProject.m
@@ -1,8 +1,30 @@
 function CreateProject(self)
-% Create new DeepAction project
+%CREATEPROJECT Creates new DeepActionProject
+%   CREATEPROJECT(SELF) creates project folder in SELF.PROJECTPATH and
+%   config file (if already exists, asks user whether to overwrite)
+
 if ~isfolder(self.ProjectPath)
     mkdir(self.ProjectPath)
+else
+    msg = sprintf('Project with path %s already exists!', self.ProjectPath);
+    warning(msg);
 end
 
-self.ConfigFile.CreateConfigFile()
+configFilePath = fullfile(self.ProjectPath, 'config.txt');
+
+if isfile(configFilePath)
+    prompt = 'Configuration file already exists, do you want to overwrite it? (y/n) ';
+    response = input(prompt, 's');
+    if strcmpi(response, 'n') || strcmp(response, 'no')
+        return
+    elseif strcmpi(response, 'y') || strcmp(response, 'yes')
+        delete(configFilePath)
+    else
+        fprintf('Invalid response! Returning...')
+        return
+    end
+else
+    self.ConfigFile.CreateConfigFile()
+end
+
 end
