7547cd5d52a83597270b1be2be2563925a2e1990
carlwharris <50850205+carlwharris@users.noreply.github.com>
2022-06-24 11:59:43 -0400

    method documentation

--- DeepAction/@DeepActionProject/ImportVideos.m
+++ DeepAction/@DeepActionProject/ImportVideos.m
@@ -1,9 +1,23 @@
 function ImportVideos(self, importTable, varargin)
-% Import videos contained in 'importTable' into project
-% 
-% importTable   - table with data to input
-%   First column - name corresponding to video in row
-%   Second column - path to video
+%IMPORTVIDEOS Import new videos into project
+%   IMPORTVIDEOS(SELF, IMPORTTABLE) imports new videos, with video names
+%   and paths contained in IMPORTTABLE, into the project
+%
+%   IMPORTTABLE(:,1) contains the name of the video in each row
+%
+%   For single-camera projects...
+%       IMPORTTABLE(:,2) contains the path to the video corresponding to
+%       the video name in the first column
+%   For multi-camera projects...
+%       IMPORTTABLE(, 2:) has multiple columns, each corresponding to the 
+%       path to the video for a particular camera. The camera name is
+%       denoted by the column header
+%
+%   If SIZE(IMPORTTABLE,2) > 2, the project is automatically designated
+%   as multi-cam. The MULTIPLECAMERAS and CAMERANAME parameters in the 
+%   project config file are automatically updated, and the PRIMARYCAMERA
+%   parameter is set to the camera name corresponding to the entries in 
+%   IMPORTTABLE(:,2)
 
 videosFolder = fullfile(self.ProjectPath, 'videos');
 
