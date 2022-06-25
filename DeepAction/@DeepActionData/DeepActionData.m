3db3ec77ff112885b411a9ef70220846f1f61d26
carlwharris <50850205+carlwharris@users.noreply.github.com>
2022-06-25 16:23:28 -0400

    create DeepActionData class

---
+++ DeepAction/@DeepActionData/DeepActionData.m
@@ -0,0 +1,16 @@
+classdef DeepActionData < DeepActionProject
+    %DEEPACTIONDATA Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        Property1
+    end
+    
+    methods
+        function self = DeepActionData(projectPath)
+            self = self@DeepActionProject(projectPath)
+        end
+        
+    end
+end
+
